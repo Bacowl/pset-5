701d2215564d4b7b2c712fa3558d1eb6e757db68
Benjamin Carpenter <BCarpenter@ucvts.tec.nj.us>
2019-12-02 10:29:37 -0500

    initial commit

--- js/shapes.js
+++ js/shapes.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    // add event listeners to respond to button clicks
+    onmouseclick
 }
 
 /*
@@ -15,7 +15,7 @@ const sayHello = function() {
  */
 
 const drawRectangle = function() {
-    
+
 };
 
 /*
@@ -23,7 +23,7 @@ const drawRectangle = function() {
  */
 
 const drawColoredRectangle = function() {
-    
+
 };
 
 /*
@@ -31,7 +31,7 @@ const drawColoredRectangle = function() {
  */
 
 const drawTriangle = function() {
-    
+
 };
 
 /*
@@ -39,7 +39,7 @@ const drawTriangle = function() {
  */
 
 const drawFace = function() {
-    
+
 };
 
 /*
@@ -47,5 +47,5 @@ const drawFace = function() {
  */
 
 const drawPyramid = function() {
-    
+
 };
